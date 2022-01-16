5de542158eda3a3da87bc9b277530dc76c07066e
duttrohan0302 <duttrohan0302@gmail.com>
2022-01-16 19:35:33 +0530

    added build

---
+++ client/build/precache-manifest.805176987b3ec7036fc3d2f844c02312.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ede528f46f3a19fa13281be933f60291",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99e5dce87d8f5ce5982a",
+    "url": "/static/css/2.7462bff2.chunk.css"
+  },
+  {
+    "revision": "7cb8c76c733bba5224ba",
+    "url": "/static/css/main.c78343b4.chunk.css"
+  },
+  {
+    "revision": "99e5dce87d8f5ce5982a",
+    "url": "/static/js/2.22306736.chunk.js"
+  },
+  {
+    "revision": "c3a68e4bf0f301f1f13ee0685407b239",
+    "url": "/static/js/2.22306736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cb8c76c733bba5224ba",
+    "url": "/static/js/main.3413ad15.chunk.js"
+  },
+  {
+    "revision": "5296e201d77e982c6a86",
+    "url": "/static/js/runtime-main.dedd8f1d.js"
+  },
+  {
+    "revision": "e9703869fa131460c3c8cf4bbf4bee14",
+    "url": "/static/media/doctor.e9703869.jpg"
+  },
+  {
+    "revision": "28fd56b4723e4ab6dea5edca4179e4f3",
+    "url": "/static/media/doctorPatientSignIn.28fd56b4.jpg"
+  },
+  {
+    "revision": "723a53e5a8982194e19a46322591df88",
+    "url": "/static/media/homepage.723a53e5.jpg"
+  },
+  {
+    "revision": "f5f2035fb262971c4566f419dedab345",
+    "url": "/static/media/patient.f5f2035f.jpg"
+  }
+]);
